84313f0f0f81421fdbacbcc025558f5015f64823
faso <yigbot@gmail.com>
2016-12-23 15:06:54 +0200

    REPL enabled

--- lang/Program.cs
+++ lang/Program.cs
@@ -1,11 +1,8 @@
-﻿using lang.Lexing;
+using lang.Lexing;
+using Lang.REPL;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lang
 {
@@ -13,30 +10,10 @@ namespace lang
     {
         static void Main(string[] args)
         {
-            string input;
-            using (StreamReader sr = new StreamReader("../../tests/test1.txt"))
-                input = sr.ReadToEnd();
+            Console.WriteLine("TrashLang v0.000000420");
+            REPL.Start();
 
-            var lexer = new Lexer()
-            {
-                input = input,
-                position = 0,
-                readPosition = 0
-            };
-
-            var watch = new Stopwatch();
-            watch.Start();
-            var tokens = lexer.Lex();
-            watch.Stop();
-            Console.WriteLine($"Lexing successful!\nTime elapsed: {watch.Elapsed.TotalSeconds}\n");
-            
-
-            foreach (var tok in tokens)
-            {
-                Console.WriteLine($"{Enum.GetName(typeof(TokenType), tok.Type).PadRight(12)}{tok.Literal}");
-            }
-
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
